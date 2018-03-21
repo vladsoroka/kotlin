9bb4e0befd5947c147b207c2d44dca53dc9f4b77
Pavel V. Talanov <talanov.pavel@gmail.com>
2018-03-21 19:46:54 +0100

    TODOS: Rewrite QuickFixMultiModuleTest to be generated

--- idea/testData/multiModuleQuickFix/sealed/header/header.kt
+++ idea/testData/multiModuleQuickFix/sealed/header/header.kt
@@ -1,4 +1,4 @@
-// "Create actual class for module js (JS)" "true"
+// "Create actual class for module testModule_JavaScript (JS)" "true"
 
 expect sealed class <caret>Sealed {
     object Obj : Sealed
